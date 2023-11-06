fc840257881c2ef64691394145c88c290358907e
dox4 <dox4@foxmail.com>
2023-11-06 23:45:15 +0800

    feat: add some self config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local opt = vim.opt
+
+opt.colorcolumn = "120" -- set ruler
+opt.mouse = "" -- disable mouse
+opt.relativenumber = false -- disable relativenumber
+opt.tabstop = 4 -- number of spaces tabs count for
