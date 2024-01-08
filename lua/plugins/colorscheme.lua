eb6e909bdb1fde9ec4ae7501a536a54680988c79
dox4 <dox4@foxmail.com>
2024-01-09 00:36:10 +0800

    feat: use random theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,83 @@
+local function random_colorscheme()
+    local themes = {
+        "dracula",
+        "tokyonight",
+        "catppuccin-mocha",
+        "monokai-pro",
+        "onedark",
+    }
+    math.randomseed(os.time())
+    local index = math.random(#themes)
+    local theme = themes[index]
+    vim.notify("set random theme: " .. theme, vim.log.levels.INFO)
+    return theme
+end
+
+local theme = random_colorscheme()
+
 return {
     { "dracula/vim" },
     {
+        "folke/tokyonight.nvim",
+        lazy = true,
+        opts = {
+            style = (function()
+                if math.random() > 0.5 then
+                    return "moon"
+                else
+                    return "night"
+                end
+            end)(),
+        },
+    },
+    {
+        "catppuccin/nvim",
+        opts = {
+            transparent_background = true,
+        },
+    },
+    {
+        "loctvl842/monokai-pro.nvim",
+        opts = {
+            filter = (function()
+                local filters = {
+                    "classic",
+                    "octagon",
+                    "pro",
+                    "machine",
+                    "ristretto",
+                    "spectrum",
+                }
+                return filters[math.random(#filters)]
+            end)(),
+        },
+    },
+    {
+        "navarasu/onedark.nvim",
+        opts = {
+            style = (function()
+                local styles = {
+                    "dark",
+                    "darker",
+                    "cool",
+                    "deep",
+                    "warm",
+                    "warmer",
+                }
+                return styles[math.random(#styles)]
+            end)(),
+        },
+    },
+    {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "dracula",
+            colorscheme = theme,
         },
     },
     {
         "nvim-lualine/lualine.nvim",
         opts = function(_, opts)
-            opts.options.theme = "dracula"
+            opts.options.theme = theme
         end,
     },
 }
